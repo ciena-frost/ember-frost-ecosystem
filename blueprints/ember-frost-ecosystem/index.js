236cebb81afa1f34a4ca7b421fbce36507ea9b74
David Fortin <dafortin@ciena.com>
2016-09-16 15:18:35 -0400

    Add lts file and blueprint installing ember-cli-ember-ecosystem

---
+++ blueprints/ember-frost-ecosystem/index.js
@@ -0,0 +1,12 @@
+/*jshint node:true*/
+module.exports = {
+  description: '',
+  normalizeEntityName: function () {
+    // this prevents an error when the entityName is
+    // not specified (since that doesn't actually matter
+    // to us
+  },
+  afterInstall: function () {
+    return this.addAddonsToProject({ packages: [{name: 'ember-cli-ember-ecosystem'}] })
+  }
+};
